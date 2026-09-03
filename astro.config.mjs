9a37ce9012c6134a91ed8af06c88181a78b65e54
kyranjamie <opensource@kyranjamie.com>
2026-09-03 18:26:51 +0200

    feat: refresh personal site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,22 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig, fontProviders } from 'astro/config'
 
 export default defineConfig({
   publicDir: './static',
   site: 'https://kyranjamie.com',
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: 'Commit Mono',
+      cssVariable: '--font-commit-mono',
+      options: {
+        variants: [
+          {
+            src: ['./src/assets/fonts/CommitMono-VF.woff2'],
+            weight: '200 700',
+            style: 'normal',
+          },
+        ],
+      },
+    },
+  ],
 })
